bdeb7b68fa26697535e2c1325eeda69cd1d9766f
Romaric Pighetti <romaric.pighetti@gmail.com>
2015-04-14 22:45:53 +0200

    Added many to many relationship between Users and Projects

--- database/relations.js
+++ database/relations.js
@@ -4,8 +4,10 @@ var User = require('../tables/user.js'),
 
 exports.init = function() {
   User.schema.hasMany(Role.schema); // a user has different roles
+  User.schema.hasMany(Project.schema, {through: Role.schema});
 
   Project.schema.hasMany(Role.schema); // a project has different roles
+  Project.schema.hasMany(User.schema, {through: Role.schema});
 
   Role.schema.belongsTo(User.schema); // a role belongs to a user
   Role.schema.belongsTo(Project.schema); // a role belongs to a project
